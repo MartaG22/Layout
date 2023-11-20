45d9201f2b56ef1c1b50bbc22e8896d32b5b6978
M G <martaxgarijo@gmail.com>
2023-11-20 18:04:00 +0100

    tarting to implement the Calculator class with its methods

---
+++ otrosEjercicios/Calculator/CLIENT/public/js/numbers.js
@@ -0,0 +1,67 @@
+// const express = require("express");
+// const app = express();
+
+
+// let enterNumber
+// console.log("HOLA")
+
+
+                
+                
+                
+let screen_operand = parseFloat(document.querySelector("[data-screen-operand]").textContent);
+let screen_result = parseFloat(document.querySelector("[data-screen-result]").textContent);
+console.log("aa:", screen_operand, "bb:", screen_result, )
+
+                
+function number1(){
+        enterNumber = 1;
+        operands.innerHTML = enterNumber;
+        console.log("NUMBER", enterNumber);
+        console.log("HOLA")
+}
+
+function number2(){
+        enterNumber = 2;
+}
+
+function number3(){
+        enterNumber = 3;
+}
+
+function number4(){
+        enterNumber = 4;
+}
+
+function number5(){
+        enterNumber = 5;
+}
+
+function number6(){
+        enterNumber = 6;
+}
+
+function number7(){
+        enterNumber = 7;
+}
+
+function number8(){
+        enterNumber = 8;
+}
+
+function number9(){
+        enterNumber = 9;
+}
+
+function number0(){
+        enterNumber = 0;
+}
+
+function point() {
+        enterPoint = ",";
+}
+
+// console.log("NUMBER", enterNumber);
+// module.exports = number1;
+
+
